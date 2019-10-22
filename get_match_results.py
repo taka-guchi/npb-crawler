3c9a0560d327242806f428eb28caa1adc4255d45
taka-guchi <takahiro89baka@gmail.com>
2019-10-22 21:12:11 +0900

    コマンドラインからスクリプトとして実行された場合にのみ処理を実行する

--- get_match_results.py
+++ get_match_results.py
@@ -8,61 +8,62 @@ import datetime
 import csv
 
 URL_TEMPLATE = 'http://baseballdata.jp/{index}/GResult.html'
-FILENAME_TEMPLATE = 'csv/{year}/{year}_{team_capital}_match_results.csv'
-
-# 現在年を取得する
-this_year = datetime.date.today().year
-
-# ブラウザのオプションを格納する変数を取得する
-options = Options()
-# Headlessモードを有効にする
-# →コメントアウトするとブラウザが実際に立ち上がる
-options.set_headless(True)
-# ブラウザを起動する
-driver = webdriver.Chrome(chrome_options=options)
-
-# サイト内のチームindexと対応するチーム名（頭文字）の辞書を作成する
-dict_teams = {1:'G',2:'S',3:'DB',4:'D',5:'T',6:'C',
+FILENAME_TEMPLATE = 'csv/{year}/{year}_{team}_match_results.csv'
+THIS_YEAR = datetime.date.today().year
+# サイト内のチームindexと対応するチーム名（頭文字）の辞書
+DICT_TEAMS = {1:'G',2:'S',3:'DB',4:'D',5:'T',6:'C',
               7:'L',8:'F',9:'M',11:'Bs',12:'H',376:'E'}
 
-for key, value in dict_teams.items():
-    # チームごとにurlを作成する
-    url = (URL_TEMPLATE.format(index=key))
+def set_options():
+    # ブラウザのオプションを格納する変数を取得する
+    options = Options()
+    # Headlessモードを設定する
+    options.set_headless(True)
 
-    # CSVファイルの設定
-    csv_file = open(FILENAME_TEMPLATE.format(year=this_year,team_capital=value),
-                    'wt', newline = '', encoding = 'utf-8')
-    writer = csv.writer(csv_file)
+    return options
 
+def write_to_csv(driver, writer):
+    # スクレイピング対象のtrクラス名(「全て見る」リンク押下後は空になる)
+    tr_class = "deftr"
+    # 「全て見る」リンクがあればクリックする
+    link_all_show = driver.find_elements(By.XPATH, "//div[@class='allshow']")
+    if len(link_all_show) > 0:
+        driver.find_element_by_class_name("allshow").click()
+        tr_class = ""
+        sleep(1)
+
+    # HTMLの文字コードをUTF-8に変換して取得
+    html = driver.page_source.encode("utf-8")
+    soup = BeautifulSoup(html,'html.parser')
+    for row in soup.findAll("tr", class_=tr_class):
+        csv_row = []
+        for cell in row.findAll("td", bgcolor=""):
+            csv_row.append(cell.get_text().strip())
+        # 空行は無視する
+        if csv_row:
+            writer.writerow(csv_row)
+
+
+def main():
     try:
-        # ブラウザでアクセスする
-        driver.get(url)
-        # スクレイピング対象trのクラス名
-        # => 「全て見る」リンク押下後はクラス名が空になるため変数で保持する
-        tr_class = 'deftr'
-        # 「全て見る」リンクが画面に存在すればを押下して全データを表示させる
-        link_all_show = driver.find_elements(By.XPATH, "//div[@class='allshow']")
-        if len(link_all_show) > 0:
-            driver.find_element_by_class_name('allshow').click()
-            tr_class = ''
-            sleep(1)
+        driver = webdriver.Chrome(chrome_options=set_options())
+        driver.set_page_load_timeout(5)
 
-        # HTMLの文字コードをUTF-8に変換して取得する
-        html = driver.page_source.encode('utf-8')
-        soup = BeautifulSoup(html,'html.parser')
+        for index, team in DICT_TEAMS.items():
+            filename = FILENAME_TEMPLATE.format(year=THIS_YEAR, team=team)
+            with open(filename, "wt", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                # チームごとの試合結果掲載ページを開く
+                url = URL_TEMPLATE.format(index=index)
+                driver.get(url)
+                sleep(1)
+                # スクレイピング => 試合結果をcsvファイルに書き出す
+                write_to_csv(driver, writer)
 
-        for row in soup.findAll('tr', class_=tr_class):
-            csv_row = []
-            for cell in row.findAll('td', bgcolor=''):
-                csv_row.append(cell.get_text().strip())
-            # 空行は無視する
-            if csv_row:
-                writer.writerow(csv_row)
+            print("success team={team}".format(team=team))
 
-        print('success team={team_capital}'.format(team_capital=value))
     except Exception as e:
-        print('error_message:{message}'.format(message=e))
-    finally:
-        csv_file.close()
+        print("error_message:{message}".format(message=e))
 
-    sleep(1)
+if __name__ == "__main__":
+    main()
